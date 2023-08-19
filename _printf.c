9aba2eb15a6c421a456aab3de47947104613c5fb
SphaxWare <itssphax123@gmail.com>
2023-08-19 09:24:46 -0700

    Modified _printf to handle %s %d and %i conversion specifiers

--- _printf.c
+++ _printf.c
@@ -11,44 +11,84 @@
  */
 int _printf(const char *format, ...)
 {
-	int id, cj, cnt = 0;
-	va_list lkm;
-	char c, *string;
+	int i, j, k, integer, temp, count = 0;
+	va_list list;
+	char c, *str, array[12];
 
-	va_start(lkm, format);
-
-	if (format == NULL || format[0] == '%' && format[1] == '\0')
+	va_start(list, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-
 	if (format[0] == '%' && format[1] == ' ' && format[2] == '\0')
 		return (-1);
-
-	for (id = 0; format[id] != '\0'; id++)
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[id] == '\0')
+		if (format[i] == '\0')
 			break;
-
-		if (format[id] == '%' && format[id + 1] == 'c')
+		if (format[i] == '%' && format[i + 1] == 'c')
 		{
-			c = va_arg(lkm, int);
+			c = va_arg(list, int);
 			_putchar(c);
-			cnt++;
-			id++;
+			count++;
+			i++;
 		}
-		else if (format[id] == '%' && format[id + 1] == '%')
+		else if (format[i] == '%' && format[i + 1] == '%')
 		{
 			_putchar('%');
-			cnt++;
-			id++;
+			count++;
+			i++;
+		}
+		else if (format[i] == '%' && (format[i + 1] == 'd' || format[i + 1] == 'i'))
+		{
+			integer = va_arg(list, int);
+			if (integer == 0)
+			{
+				_putchar(48);
+				count++;
+			}
+			k = 0;
+			while (integer != 0)
+			{
+				if (integer < 0)
+				{
+					_putchar('-');
+					integer = -integer;
+					count++;
+				}
+				else
+				{
+					temp = integer % 10;
+					integer /= 10;
+					array[k] = temp + 48;
+					count++;
+					k++;
+				}
+			}
+			for (; k >= 1; k--)
+			{
+				_putchar(array[k - 1]);
+			}
+			i++;
+		}
+		else if (format[i] == '%' && format[i + 1] == 's')
+		{
+			str = va_arg(list, char *);
+			for (j = 0; str[j] != '\0'; j++)
+			{
+				if (str[j] == '\0')
+					break;
+				_putchar(str[j]);
+				count++;
+			}
+			i++;
 		}
 		else
 		{
-			_putchar(format[id]);
-			cnt++;
+			_putchar(format[i]);
+			count++;
 		}
 	}
 
-	va_end(lkm);
+	va_end(list);
 
-	return (cnt);
+	return (count);
 }
